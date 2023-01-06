dc612ca989ff7577bd875065b12fe8e244252780
mersaucedo <mersaucedosantos@gmail.com>
2023-01-06 10:09:51 -0500

    template

---
+++ src/app/template/contact.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+interface ContactForm
+  {
+    "name": string,
+    "checkAdult": boolean;
+    "department": string;
+    "comment": string;
+  }
+   
+
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  model= {
+    "name": "",
+      "checkAdult": false,
+      "department": "",
+      "comment": ""
+    
+     
+  }
+  ngOnInit(): void {
+  }
+
+  onSubmit(form:NgForm):void{
+    console.log('Form values', form);
+  }
+
+}
